fa0d7e8c836d16ed8df7aba20b3a801c2e37ac6a
Roman <roman.boxes@gmail.com>
2026-02-01 15:29:17 +0100

    exercise 3 total function and toFixed method
    - total function to get the total
    - toFixed method to return specified number of decimal places. In this case 2.

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,12 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+function Total(order) {
+  let total = 0;
+  for (let i = 0; i < order.length; i++) {
+    total += order[i].quantity * order[i].unitPricePence;
+  }
+  return (total / 100).toFixed(2);
+}
+
+console.log(`Total: ${Total(order)}`); 
